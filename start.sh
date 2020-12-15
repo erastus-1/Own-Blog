223d7429fa6ddcad43ed598642e948a34d71e628
erastus <erastuskariuki15@gamil.com>
2020-12-15 10:52:21 +0300

    make change

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 export SECRET_KEY='mutwech'
 export MAIL_USERNAME='erastuskariuki15@gmail.com'
 export MAIL_PASSWORD='@e1r2a3s4#'
-export DATABASE_URL='postgresql+psycopg2://erastus:Angular2020@localhost/blog'
 
 python3 manage.py server
